0751b040dbc451c982ba924a21a0389fa171e05b
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-10-04 09:06:36 +0300

    Frontend <> adding js boilerplate

--- dating_frontend/scripts/index.js
+++ dating_frontend/scripts/index.js
@@ -1,10 +1,54 @@
-const signup = document.getElementById("signup");
-const exit_btn = document.getElementById("x-btn");
-const popup = document.getElementById("popup")
+const dating_pages = {};
 
-const login_signup_toggle = () => {
-    popup.classList.toggle("hide")
+dating_pages.baseURL = "http://127.0.0.1:8000/api/";
+
+dating_pages.Console = (title, values, oneValue = true) => {
+    console.log('---' + title + '---');
+    if(oneValue){
+        console.log(values);
+    }else{
+        for(let i =0; i< values.length; i++){
+            console.log(values[i]);
+        }
+    }
+    console.log('--/' + title + '---');
 }
 
-signup.addEventListener("click", login_signup_toggle);
-exit_btn.addEventListener("click", login_signup_toggle);
+dating_pages.getAPI = async (api_url) => {
+    try{
+        return await axios(api_url);
+    }catch(error){
+        dating_pages.Console("Error from GET API", error);
+    }
+}
+
+dating_pages.postAPI = async (api_url, api_data, api_token = null) => {
+    try{
+        return await axios.post(
+            api_url,
+            api_data,
+            { headers:{
+                    'Authorization' : "token " + api_token
+                }
+            }
+        );
+    }catch(error){
+        dating_pages.Console("Error from POST API", error);
+    }
+}
+
+dating_pages.loadFor = (page) => {
+    eval("dating_pages.load_" + page + "();");
+}
+
+dating_pages.load_registration = async () => {
+    const landing_url = `${dating_pages.baseURL}/landing`;
+    const response_landing = await dating_pages.getAPI(landing_url);
+    dating_pages.Console("Testing Products API", response_landing.data.data);
+
+
+}
+
+dating_pages.load_products = () => {}
+
+
