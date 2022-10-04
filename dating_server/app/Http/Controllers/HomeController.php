69b7245767931f28eb6f5ac937e62da43e44a80c
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-10-05 02:29:05 +0300

    Backend <> adding add or get favorites in same function and its route

--- dating_server/app/Http/Controllers/HomeController.php
+++ dating_server/app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Favorite;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
 
@@ -17,6 +18,29 @@ class HomeController extends Controller{
             "Status" => "Success",
             "response" => $interested_id
         ]);
+    }
+
+    function addOrGetFavorites(Request $request, $id = "add") {
+        if($id == "add") {
+            $favorite = new Favorite;
+        }else {
+            $favorite = Favorite::select($id);
+        }
+
+        $favorite->id = $request->id ? $request->id : $favorite->id;
+        // $favorite->category_id = $request->category_id? $request->category_id : $favorite->category_id;
+        if($favorite->save()){
+            return response()->json([
+                "status" => "Success",
+                "data" => $favorite
+            ]);
+        }
+
+        return response()->json([
+            "status" => "Error",
+            "data" => "Error creating a model"
+        ]);
+    
     }
 
 
