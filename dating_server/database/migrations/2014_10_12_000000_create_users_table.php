4e50a010b15addb4106452f293b4b0c079fa0719
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-10-04 09:39:55 +0300

    Merging

---
+++ dating_server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('email')->unique();
+            $table->string('gender');
+            $table->string('interest');
+            $table->integer('age');
+            $table->string('password');
+            $table->string('location');
+            $table->string('profile_picture');
+            $table->string('bio');
+            $table->boolean('visible')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
